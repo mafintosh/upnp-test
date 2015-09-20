2cbb87a44f3756051477258528145f3b9c863d9b
Mathias Buus <mathiasbuus@gmail.com>
2015-09-20 19:43:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# upnp-test
+
+Test if the current network allows for direct p2p connections using upnp
+
+```
+npm install -g upnp-test
+```
+
+## Usage
+
+On the network you want to test run
+
+```
+upnp-test
+```
+
+The above will print out whether or not it was able to directly to the machine over the internet using upnp
+
+## License
+
+MIT
---
+++ bin.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+var pump = require('pump')
+var upnp = require('nat-upnp')
+var net = require('net')
+
+var client = upnp.createClient()
+
+client.portMapping({
+  public: 33658,
+  private: 33658,
+  ttl: 0
+}, function (err) {
+  if (err) throw err
+
+  var server = net.createServer(function (sock) {
+    pump(sock, sock)
+  })
+
+  server.listen(33658, function () {
+    client.externalIp(function (err, ip) {
+      if (err) throw err
+
+      var client = net.connect(33658, ip)
+      var buf = ''
+
+      client.setTimeout(20 * 1000, function () {
+        client.destroy()
+      })
+
+      client.setEncoding('utf-8')
+      client.end('hello world')
+
+      client.on('error', onend)
+      client.on('close', onend)
+      client.on('end', onend)
+      client.on('data', function (data) {
+        buf += data
+      })
+
+      function onend () {
+        if (buf === 'hello world') {
+          console.log('✓ Was able to directly connect to this machine over the internet using upnp')
+          process.exit(0)
+        } else {
+          console.error('✗ Could not connect to this machine over the internet using upnp')
+          process.exit(1)
+        }
+      }
+    })
+  })
+})
