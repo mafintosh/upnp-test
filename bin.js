9fb1ab48f6069df823e0dbf5b9140e6688368f40
Mathias Buus <mathiasbuus@gmail.com>
2016-02-01 13:36:51 +0100

    better error message

--- bin.js
+++ bin.js
@@ -25,14 +25,14 @@ server.listen(port, function () {
     private: {host: host, port: port},
     ttl: 120
   }, function (err) {
-    if (err) return onend()
+    if (err) return onend(err)
 
     var server = net.createServer(function (sock) {
       pump(sock, sock)
     })
 
     client.externalIp(function (err, ip) {
-      if (err) return onend()
+      if (err) return onend(err)
       console.log('Router says external IP is %s', ip)
       publicIp.v4(function (err, ip2) {
         if (!err) console.log('OpenDNS says external IP is %s', ip2)
@@ -58,12 +58,12 @@ server.listen(port, function () {
   })
 })
 
-function onend () {
+function onend (err) {
   if (buf === 'hello world') {
     console.log('Was able to directly connect to this machine over the internet using upnp')
     process.exit(0)
   } else {
-    console.error('Could not connect to this machine over the internet using upnp')
+    console.error('Could not connect to this machine over the internet using upnp' + (err ? ' (' + err.message + ')' : ''))
     process.exit(1)
   }
 }
